840586d61edc30364c96b82166420b1af977956f
athenavi <625237127@qq.com>
2023-06-01 16:11:14 +0800

    Orgin

---
+++ precache-manifest.8bdb45646e500b79369fbde554504eee.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b581c1cef282d5dabdce",
+    "url": "./fbeb0390ce2e09e876ae.module.wasm"
+  },
+  {
+    "revision": "badf4367b8540e21f64e86b7d2f5b988",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d5107279cf53066aa073",
+    "url": "./static/css/2.c412fb8a.chunk.css"
+  },
+  {
+    "revision": "8593b5730afe27175082",
+    "url": "./static/css/main.ac869228.chunk.css"
+  },
+  {
+    "revision": "d5107279cf53066aa073",
+    "url": "./static/js/2.f131f59c.chunk.js"
+  },
+  {
+    "revision": "b581c1cef282d5dabdce",
+    "url": "./static/js/3.55dfb220.chunk.js"
+  },
+  {
+    "revision": "8593b5730afe27175082",
+    "url": "./static/js/main.cc22ee43.chunk.js"
+  },
+  {
+    "revision": "070f221f22eea867466b",
+    "url": "./static/js/runtime-main.a17b931a.js"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "./static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "./static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "./static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "./static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "./static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "./static/media/icons-20.cef8cdbb.woff"
+  }
+]);
